0550c0e087f583b5c398ee55f9f106d757fc0b60
Wanderson Luiz Corni <wanderson.corni@fiocruz.br>
2025-04-12 19:31:24 -0300

    Implementação do cadastro do cliente

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,5 @@
  */
 
 $basePath = dirname(__DIR__);
-require $basePath.'\app\access-control\routes\api.php';
-require $basePath.'\app\application\routes\api.php';
+require $basePath.'/app/access-control/routes/api.php';
+require $basePath.'/app/application/routes/api.php';
--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,5 @@
  */
 
  $basePath = dirname(__DIR__);
- require $basePath.'\app\access-control\routes\web.php';
- require $basePath.'\app\application\routes\web.php';
+ require $basePath.'/app/access-control/routes/web.php';
+ require $basePath.'/app/application/routes/web.php';
